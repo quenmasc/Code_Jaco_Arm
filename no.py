85431903f8b6e4a55b5f5c4ed75f48ceea60cb79
quenmasc <quent-200193@hotmail.fr>
2017-04-11 12:44:14 -0400

    error tab

--- no.py
+++ no.py
@@ -17,34 +17,34 @@ block_length  = 1024    # samples
 
 
 class Limiter:
-def __init__(self, attack_coeff, release_coeff, delay, dtype=float32):
-    self.delay_index = 0
-    self.envelope = 0
-    self.gain = 1
-    self.delay = delay
-    self.delay_line = zeros(delay, dtype=dtype)
-    self.release_coeff = release_coeff
-    self.attack_coeff = attack_coeff
+	def __init__(self, attack_coeff, release_coeff, delay, dtype=float32):
+		self.delay_index = 0
+		self.envelope = 0
+		self.gain = 1
+		self.delay = delay
+		self.delay_line = zeros(delay, dtype=dtype)
+		self.release_coeff = release_coeff
+		self.attack_coeff = attack_coeff
 
-def limit(self, signal, threshold):
-    for i in arange(len(signal)):
-        self.delay_line[self.delay_index] = signal[i]
-        self.delay_index = (self.delay_index + 1) % self.delay
+	def limit(self, signal, threshold):
+		for i in arange(len(signal)):
+			self.delay_line[self.delay_index] = signal[i]
+			self.delay_index = (self.delay_index + 1) % self.delay
 
         # calculate an envelope of the signal
-        self.envelope *= self.release_coeff
-        self.envelope  = max(abs(signal[i]), self.envelope)
+			self.envelope *= self.release_coeff
+			self.envelope  = max(abs(signal[i]), self.envelope)
 
         # have self.gain go towards a desired limiter gain
-        if self.envelope > threshold:
-            target_gain = (1+threshold-self.envelope)
-        else:
-            target_gain = 1.0
-        self.gain = ( self.gain*self.attack_coeff +
+			if self.envelope > threshold:
+				target_gain = (1+threshold-self.envelope)
+			else:
+				target_gain = 1.0
+			self.gain = ( self.gain*self.attack_coeff +
                       target_gain*(1-self.attack_coeff) )
 
         # limit the delayed signal
-        signal[i] = self.delay_line[self.delay_index] * self.gain
+			signal[i] = self.delay_line[self.delay_index] * self.gain
 
 
 
@@ -58,7 +58,7 @@ def limit(self, signal, threshold):
 		original_signal = array(signal, copy=True, dtype=dtype)
 		limiter = Limiter(attack_coeff, release_coeff, delay, dtype)
 
-def callback(in_data, frame_count, time_info, flag):
+	def callback(in_data, frame_count, time_info, flag):
 		if flag:
 			print("Playback Error: %i" % flag)
 		played_frames = callback.counter
@@ -77,15 +77,15 @@ def callback(in_data, frame_count, time_info, flag):
              output = True,
              stream_callback = callback)
 
- while stream.is_active():
- sleep(1)
+	while stream.is_active():
+		sleep(1)
 
- stream.close()
- pa.terminate()
+		stream.close()
+		pa.terminate()
 
 
- plt.figure()
- plt.plot(original_signal, color='green', label='original signal')
- plt.plot(signal, color='red', label='limited signal')
- plt.legend()
- plt.show(block=True)
+		plt.figure()
+		plt.plot(original_signal, color='green', label='original signal')
+		plt.plot(signal, color='red', label='limited signal')
+		plt.legend()
+		plt.show(block=True)
