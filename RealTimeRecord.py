c6f3a706251683e3b8b07d06b7204ba03379408f
Quentin M <quent-200193@hotmail.fr>
2017-04-14 19:40:53 +0000

    class record is finished -> if mistake say it to mee

--- RealTimeRecord.py
+++ RealTimeRecord.py
@@ -7,7 +7,7 @@ import struct
 import array
 import time
 
-class Record :
+class Record(object) :
     """Initialize audio buffer"""
     def __init__(self) :
         self.__rate=16000 # sample rate
@@ -18,7 +18,8 @@ class Record :
     """"Reads audio from ALSA audio device """
     def __read(self) :
         card='sysdefault:CARD=Device'
-        inp = alsa.PCM(alsa.PCM_CAPTURE, alsa.PCM_NORMAL,card)
+        inp = alsa.PCM(
+alsa.PCM_CAPTURE, alsa.PCM_NORMAL,card)
         inp.setchannels(1)
         inp.setrate(self.__rate)
         inp.setformat(alsa.PCM_FORMAT_S32_LE)
@@ -38,15 +39,42 @@ class Record :
     def read(self):
         return self.__read_queue.get() , self.__read_frame.get()
 
-audio= Record()
-audio.run()
-timeout=0
-start=time.time()
-while timeout<=10 :
-    end=time.time()
-    data , length = audio.read()
-    print len(data) , length
-    timeout=end-start
+
+    def push_up(self,buffer):
+        return buffer
+       
+
+    def read_data(self):
+        print("Reading data")
+        buffer=[[],[],[]]
+        i=0
+        while True:
+                raw_data=b''
+                while len(raw_data)<2*self.__rate :
+                    data, length = audio.read()
+                    if length >0 :
+                        raw_data+=data
+                if buffer[i]!=[] :
+                    print "Buffer ", i ," is not empty -> fatal error ! "
+                    break
+                else :
+                    buffer[i]=np.fromstring(raw_data[:2*self.__rate],
+                                        dtype=np.int16)
+                yield buffer[i]
+                buffer[i]=[]
+                i+=1
+                i=i%3
+               # print(len(np.fromstring(raw_data[:2*rate],dtype=np.int16)))
+               # print(np.fromstring(raw_data[:2*rate],dtype=np.int16))
+
+
+if __name__=='__main__' :
+    audio= Record()
+    audio.run()
+    r=audio.read_data()
+    while True :
+        print(r.next())
+        print("end of transmission -> waiting new data")
     
         
     
