d3e541a7eecdc7c3090d7c38f8cb67579a52d6d7
Quentin M <quent-200193@hotmail.fr>
2017-04-21 20:56:37 +0000

    processes run -> next step : two read for 1 write in process

--- alsa_record.py
+++ alsa_record.py
@@ -1,6 +1,6 @@
 # all import here
 import alsaaudio as alsa
-from multiprocessing import Process, Queue 
+from multiprocessing import Process, Queue , Pool , Lock
 import numpy as np
 import math
 import struct
@@ -23,6 +23,8 @@ class Record(object) :
         self.__read_queue = Queue()
         self.__read_frame = Queue()
         self.__write_queue=Queue()
+        self.__RingBufferWrite_queue=Queue()
+        self.__RingBufferRead_queue=Queue()
 	# params  
 	self.__format=alsa.PCM_FORMAT_S16_LE # format of sample 
 	self.__byte =4 # size of each sample 
@@ -51,6 +53,7 @@ class Record(object) :
         inp.setperiodsize(self.__rate / 50) # buffer period size
         print("Audio Device is parameted")
         
+
         while True :
             frame_count, data = inp.read()  # process to get all value from alsa buffer -> period size * bytes per sample
             self.__read_queue.put(data) # put data in queue -> string type
@@ -66,6 +69,7 @@ class Record(object) :
 
         while True:
             data = self.__write_queue.get()
+
             outp.write(data)
 
 
@@ -78,16 +82,22 @@ class Record(object) :
 
 
             
+
     """ Run proccesses """
     def run(self):
-        self.__pre_post_data()
+        #self.__pre_post_data()
+       # index=Queue()
         read_process = Process(target=self.__read)
         write_process = Process(target = self.__write)
+        ring=RingBuffer.RingBuffer(RingLength,window_sample,step_sample)
+        RingBuffer_write_process = Process (target =self.__RingBufferWrite, args=(ring,))
+       # RingBuffer_read_process = Process (target=self.__RingBufferRead, args=(ring,))
         read_process.start()
+        RingBuffer_write_process.start()
+       # RingBuffer_read_process.start()
         write_process.start()
-
-
-    
+        
+        
     """ get all data from audiuo devices """
     def read(self):
         return self.__read_queue.get() , self.__read_frame.get()     
@@ -98,6 +108,7 @@ class Record(object) :
 
     # Pseudonymize the audio samples from a binary string into an array of integers
     def pseudonymize(self, s):
+
         sl=len(s)/self.__byte
         return struct.unpack('<%dh' % sl,s)
     #np.fromstring(s[:2*8000], dtype=np.uint16)
@@ -110,36 +121,70 @@ class Record(object) :
 
         return s
 
+    """  Ring Buffer -> READ AND WRITE METHODS """
+    def __RingBufferWrite(self,ring):
+        flag=0
+        while True :
+            data=self.__RingBufferWrite_queue.get()
+            ring.extend(data)
+            if flag==2 :
+                temp =ring.get()
+            else :
+                flag+=1
+                temp=np.zeros(200)
+            self.__RingBufferRead_queue.put(temp)
+
+    def RingBufferWrite(self,data):
+        self.__RingBufferWrite_queue.put(data)
+        
+    def __RingBufferRead(self,ring):
+        flag=0
+        while True :
+           # print("in")
+            if flag==0:
+                temp=0
+                flag=1
+            else :
+                temp=ring.get()
+          #  print(temp)
+            self.__RingBufferRead_queue.put(temp)
+
+    def RingBufferRead(self):
+        return self.__RingBufferRead_queue.get()
     
+
 if __name__=='__main__' :
     audio= Record()
     RingLength=24650
     window_sample=200
-    step_sample=80
-    buff=RingBuffer.RingBuffer(RingLength,window_sample,step_sample)
+    step_sample=85
+   # store=RingBuffer.WaitingBuffer(10,window_sample)
     audio.run()
     cur=0
     tail=0
-    i=0
+    i=0 
+    c=[[],[]]
     start=time.time()
     while True :
         data, length = audio.read()
-        if length>0 :
-            pdata=audio.pseudonymize(data)
-            #print(pdata)
-            ndata=DSP.normalize(pdata,0xFFFF)
-            #print(ndata)
-            buff.extend(ndata)
-            a=buff.index()
-            if a> window_sample :
-               c=buff.get()
-               i+=1
-               print(i)
-            stop=time.time()
-            print(stop-start)
-           # ndata=DSP.denormalize(ndata,0xFFFF)
-           # ndata=audio.depseudonymize(pdata)
-           # audio.write(ndata)
+        #if length>0 :
+        pdata=audio.pseudonymize(data)
+        ndata=DSP.normalize(pdata,0xFFFF)
+        audio.RingBufferWrite(ndata)
+        c=audio.RingBufferRead()
+        print(c)
+           # buff.extend(ndata)
+           # a=buff.index()
+           # if a> window_sample :
+           #     j=0
+           #     while j<2 :
+           #        c[j]=buff.get()
+           #        j+=1  
+        stop=time.time()
+       # print(stop-start)
+            #ndata=DSP.denormalize(ndata,0xFFFF)
+        ndata=audio.depseudonymize(pdata)
+        audio.write(ndata)
 #
     print("out of loop")
     print("end of transmission -> waiting new data")
