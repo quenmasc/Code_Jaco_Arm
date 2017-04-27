add new function but background update doesn't work -> wrong value and detection failures

function.py
@@ -10,12 +10,22 @@ import os
 import scipy
 
 def sigmoid(Lambda,x):
-    return 1/(5+math.exp(-Lambda*x)) # expit also allows it faster than mine.
+       return 1/(5+math.exp(-Lambda*x)) # expit also allows it faster than mine.
 
-def correlation_1D(x,x_noise)
+    
+def correlation_1D(x,x_noise):
     return scipy.spatial.distance.correlation(x,x_noise)
 
-def correlation_ND(x,x_noise):
-    
+def distance(x,x_noise):
+    return math.fabs(x-x_noise)
 
-def update(x,x_old, p):
+def updateMFCCsNoise(x,x_noise, p):
+    # x and x_noise need to be an np.array
+      return p*x_noise+(1-p)*x
+
+#def updateEntropyThreshold(x,x_noise,p):
+
+
+def MeanStandardDeviation(x, Lambda):
+    return np.mean(x)+Lambda*np.std(x)
+