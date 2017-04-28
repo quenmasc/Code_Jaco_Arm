e8086e7a2e468e2a8024757daa75263235072f46
Quentin M <quent-200193@hotmail.fr>
2017-04-28 18:42:19 +0000

    add deltaMFCCs method

--- function.py
+++ function.py
@@ -8,6 +8,9 @@ import DSP
 import wave
 import os
 import scipy
+import numpy.matlib as mat
+from Tkinter import END
+from scipy.signal import lfilter
 
 __author__="Quentin MASCRET <quentin.mascret.1@ulaval.ca>"
 __date__="2017-04-26"
@@ -31,3 +34,14 @@ def updateMFCCsNoise(x,x_noise, p):
 def MeanStandardDeviation(x, Lambda):
     return np.mean(x)+Lambda*np.std(x)
 
+
+def deltaMFCCs(MFCCs,w):
+       hlen=int(np.floor(w/2))
+       win=np.linspace(4,-4,w)
+       x_b=mat.repmat(MFCCs[:,0],hlen,1).T
+       x_e=mat.repmat(MFCCs[:,(MFCCs.size/len(MFCCs))-1],hlen,1).T 
+       x_mfcc=np.array(MFCCs)
+       xx=np.concatenate((x_b,x_mfcc,x_e),axis=1)
+       d=lfilter(win,1,xx,axis=1)
+       last_step=d[:,2*hlen+np.arange(MFCCs.size/len(MFCCs))]
+       return last_step
