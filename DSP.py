43128bf00273cf826cd75511638c8e6de0f9ea3f
Quentin M <quent-200193@hotmail.fr>
2017-05-01 18:34:51 +0000

    change denormalization

--- DSP.py
+++ DSP.py
@@ -7,7 +7,7 @@ import struct
 import array
 import time
 import numpy.fft
-
+from scipy.signal import hilbert
 
 __author__="Quentin MASCRET <quentin.mascret.1 ulaval.ca>"
 __date__="2017-04-14"
@@ -17,9 +17,9 @@ THRESHOLD=0.1
 """ Normalize the audio samples from an array of integers"""
 def normalize(data,max_value) :
     data=np.array(data)
-  #  biais=int(0.5 * max_value)
+    #biais=int(0.5 * max_value)
     fac=1.0/(max_value)
-    data=fac * (data)# - biais )
+    data=fac * (data)#- biais )
     return data
 
 def subframe(data,window_ms=0.025,step_ms=0.01, rate=16000) :
@@ -36,6 +36,20 @@ def threshold(snd_data):
     # return 'true' if below the 'silent' threshold
     return np.max(snd_data) > THRESHOLD
 
+def EndSegments(cond,previous_amplitude_envelope,currIndex,tail, AudioSample):
+    current_amplitude_envelope=np.sum(np.abs(hilbert(AudioSample)))
+    print "prev", previous_amplitude_envelope , "curr" , current_amplitude_envelope
+    if  previous_amplitude_envelope >= current_amplitude_envelope and cond<=3:
+        print("true")
+        currTail=currIndex
+    else :
+        currTail=tail
+        cond+=1
+    return cond, currTail , current_amplitude_envelope
+
+
+
+    
 def logEnergy(frame):
     nfft=256 # the nextpow of 200 samples
     fft = numpy.fft.rfft(frame, nfft)
@@ -47,7 +61,7 @@ def logEnergy(frame):
     Denormalize the data from an array of floats with unity level into an array of integers.
     """
 def denormalize(data, max_val):
-        #bias = int(0.5 * max_val)
+       # bias = int(0.5 * max_val)
         fac = max_val
         data = np.array(data)
         data = (fac * data)#.astype('H') + bias
