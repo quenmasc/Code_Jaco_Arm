c18b76fec5791d9ac85954b52c7dd157b09701ac
Quentin M <quent-200193@hotmail.fr>
2017-05-16 21:44:32 +0000

    cpp main work

--- cpp/PythonBridge.h
+++ cpp/PythonBridge.h
@@ -6,6 +6,9 @@
 class PythonBridge {
 	public :
 		PythonBridge();
+		~PythonBridge();
 		void finalize_python();
 		void Running_python();
+		int ClassValue();
+		void ClassValue2();
 };
--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,7 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <iostream>
 #include "PythonBridge.h"
+#include <thread>
+PythonBridge Bridge ;
 
-int main(){
-	return 0;
+void foo(){
+	PyGILState_STATE gstate=PyGILState_Ensure();
+	Bridge.Running_python();
+	PyGILState_Release(gstate);
 }
+void fol(){
+	Bridge.ClassValue2();
+}
+void mess (){
+	while(1){
+			sleep(2);
+			printf("oui");
+	}
+}
+int main(){
+	std:: thread first(foo);
+	std:: thread second(fol);
+	std:: thread third(mess);
+	first.join();
+	second.join();
+	third.join();
+	return 0 ;
+	
+}
+
